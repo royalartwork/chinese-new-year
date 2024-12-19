f6c7506bf10a6476f114e84b4aee1084821b9c75
royalartwork <royalartwork8@gmail.com>
2024-12-19 11:15:48 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: "JUDUL",
-  tagline: "",
-  description: "",
-  keywords: "",
+  title: "Chinese New Year",
+  tagline: "DIY Your Way to a Happy Chinese New Year!",
+  description: "Get crafty for Chinese New Year! Find DIY projects, printables, and creative ideas to celebrate the festivities. Let's make this year extra special.",
+  keywords: "Chinese New Year, traditions, recipes, decorations, red envelopes, lion dance, dumplings, festive",
   author: "Milyader Muda",
   favicon: "",
   imageCount: 20,
